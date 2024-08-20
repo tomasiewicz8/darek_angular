2c850f10f5ca96c2016c2f0f8fe971de2b0c5687
Sebas <tomasiewic@gmail.com>
2024-08-20 19:36:16 +0200

    I have added styles for car of product and create the galery component

---
+++ src/app/components/galery/galery.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-galery',
+  standalone: true,
+  imports: [],
+  templateUrl: './galery.component.html',
+  styleUrl: './galery.component.scss'
+})
+export class GaleryComponent {
+
+}
